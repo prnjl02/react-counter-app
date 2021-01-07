7f7769277148443885a8dd629ca37e1bba032886
pverma93 <pverma93@in.ibm.com>
2021-01-07 11:34:25 +0530

    list rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     counter: 0,
+    tags: ["tag1", "tag2", "tag3"],
     //  imageUrl: "https://picsum.photos/200",
   };
   styles = {
@@ -19,7 +20,14 @@ class Counter extends Component {
         </span> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <span style={{ fontSize: 30 }}>Hiii</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button className="btn btn-secondary btn-sm" onClick={this.increment}>
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -33,6 +41,11 @@ class Counter extends Component {
     const { counter } = this.state;
     return counter === 0 ? "Zero" : counter;
   }
+  increment = () => {
+    let counter = this.state.counter;
+    counter++;
+    this.setState({ counter: counter });
+  };
 }
 
 export default Counter;
